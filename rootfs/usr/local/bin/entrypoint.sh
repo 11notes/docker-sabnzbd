1e2f564cbff794af06368cc775b438ba33a838d8
ElevenNotes <github@11notes.ch>
2025-09-04 10:11:17 +0200

    fix: only log to stdout no file

--- rootfs/usr/local/bin/entrypoint.sh
+++ rootfs/usr/local/bin/entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/ash
   if [ -z "${1}" ]; then
-    set -- "${APP_OPT_ROOT}/SABnzbd.py" \
-      --config-file ${APP_ROOT}/etc/default.conf \
-      --server "0.0.0.0"
+    set -- "/usr/local/bin/python" \
+      "${APP_OPT_ROOT}/SABnzbd.py" \
+      --config-file ${APP_ROOT}/etc/sabnzbd.ini \
+      --server "0.0.0.0" \
+      --browser 0 \
+      --console \
+      --logging 1 \
+      --disable-file-log
     eleven log start
   fi
 
